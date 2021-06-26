3899ade42010df18222645ed01cc2e8135fdfe0e
김동하 <kamdunga2002@gmail.com>
2021-06-26 14:43:44 +0900

    feat(App): add server with CRUD

--- src/App.vue
+++ src/App.vue
@@ -41,17 +41,45 @@ export default {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask;
     },
-    addTask(task) {
-      this.tasks = [...this.tasks, task];
+    async addTask(task) {
+      const res = await fetch(`${BASE_URL}/tasks`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task),
+      });
+      const data = await res.json();
+      this.tasks = [...this.tasks, data];
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       if (confirm('sure?')) {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        const res = await fetch(`${BASE_URL}/tasks/${id}`, {
+          method: 'DELETE',
+        });
+
+        res.status === 200
+          ? (this.tasks = this.tasks.filter(task => task.id !== id))
+          : alert('failed to delete');
       }
     },
-    toggleReminder(id) {
+
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+      const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+      const res = await fetch(`${BASE_URL}/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(updatedTask),
+      });
+
+      const data = await res.json();
+
       this.tasks = this.tasks.map(task =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       );
     },
 
@@ -62,6 +90,7 @@ export default {
         return data;
       }
     },
+
     async fetchTask(id) {
       const res = await fetch(`${BASE_URL}/tasks/${id}`);
       if (res) {
